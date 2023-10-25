ff42852cb1c1339f8e40077bccde7335efc6d26c
mac1 <mac1@MacBookPro-2.local>
2023-10-25 11:45:40 +0500

    smth

--- src/pages/public/register/index.jsx
+++ src/pages/public/register/index.jsx
@@ -1,38 +1,32 @@
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
-// import { toast } from "react-toastify";
-import Cookies from "js-cookie";
+// import { useSelector } from "react-redux";
+// // import { toast } from "react-toastify";
+// import Cookies from "js-cookie";
 
-import { authName } from "../../../redux/slice/auth";
-import { TOKEN } from "../../../constants";
+// import { authName } from "../../../redux/slice/auth";
+// import { TOKEN } from "../../../constants";
 
 // import "./style.scss";
 import request from "../../../sever";
+import { message } from "antd";
 
 const Register = () => {
-  const { isAuthenticated } = useSelector((state) => state[authName]);
-
   const navigate = useNavigate();
 
-  const submit = async (e) => {
-    e.preventDefault();
-    let user = {
-      first_name: e.target.first_name.value,
-      last_name: e.target.last_name.value,
-      username: e.target.username.value,
-      password: e.target.password.value,
-    };
+  const submit = async (user) => {
+    // e.preventDefault();
+    // let user = {
+    //   username: e.target.username.value,
+    //   password: e.target.password.value,
+    // };
     try {
-      // toast.success("Successfully registered!");
+      message.success("Successfully registered!");
       let res = await request.post("auth/register", user);
       console.log(res);
-      Cookies.set(TOKEN, res.token);
-      isAuthenticated(true);
+      message.success("You have successfully registered");
       navigate("/user");
-      console.log(res);
-      console.log(user);
     } catch (err) {
-      // toast.error("Invalid");
+      message.error(err.response.data.message);
       console.log(err);
     }
   };
